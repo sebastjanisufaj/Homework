a12d00516692f22ad742e8db5c69214fccb240ce
Sebastian Isufaj <seb.encod@gmail.com>
2022-11-28 13:45:06 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("How to make Spaghetti alla Carbonara:")
+
+let spaghetti = 400;
+let guanciale = 250;
+let eggYolk = 6;
+let pecorino = 50;
+let blackPepper = 4;
+
+console.log("\nIngredients for 4 people:");
+console.log("Spaghetti:", spaghetti, "\nGuanciale:", guanciale, "\nVery fresh egg yolks:", eggYolk, "\nAged Granted Pecorino Romano cheese:", pecorino, "\nRaw black pepper:", blackPepper);
